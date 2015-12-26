02e94b33794cd8373047e5e76d88e402851e8263
Jules <jferreira@bamboohr.com>
2015-12-25 19:53:41 -0600

    insert

--- libs/CircularDoublyLinkedList.js
+++ libs/CircularDoublyLinkedList.js
@@ -1,5 +1,32 @@
 function CircularDoublyLinkedList() {}
 
-CircularDoublyLinkedList.prototype.head = null;
+CircularDoublyLinkedList.prototype = {
+	/**
+	 * @type {Node}
+	 */
+	head: null,
+
+	/**
+	 * @type {Number} list count
+	 */
+	n: 0,
+
+	/**
+	 * insert node at 'end' of list (after most recently added node, before first)
+	 *
+	 * @param {Node} node
+	 */
+	insert: function(node) {
+		if (this.head === null) {
+			this.head = node.next = node.prev = node;
+		} else {
+			node.next = this.head;
+			node.prev = this.head.prev;
+			this.head.prev.next = node;
+			this.head.prev = node;
+		}
+		this.n++;
+	}
+}
 
 module.exports = CircularDoublyLinkedList;
--- libs/__tests__/CircularDoublyLinkedList-test.js
+++ libs/__tests__/CircularDoublyLinkedList-test.js
@@ -1,9 +1,54 @@
 jest.dontMock('../CircularDoublyLinkedList.js');
+jest.dontMock('../Node.js');
 
-describe('make list', function() {
-	it('creates a new (empty) list', function() {
-		var List = require('../CircularDoublyLinkedList');
-		var list = new List();
-		expect(list.head).toBeNull();
+describe('CircularDoublyLinkedList', function() {
+
+	var List = require('../CircularDoublyLinkedList');
+	var Node = require('../Node');
+	var list = null;
+
+	describe('constructor', function() {
+		it('creates a new (empty) list', function() {
+			list = new List();
+			expect(list.head).toBeNull();
+			expect(list.n).toBe(0);
+		});
+	});
+
+	describe('insert', function() {
+		var node1 = new Node(1);
+		var node2 = new Node(2);
+		var node3 = new Node(3);
+
+		it('adds a Node to empty list', function() {
+			list.insert(node1);
+			expect(list.head.value).toBe(1);
+			expect(list.head.next).toBe(list.head);
+			expect(list.head.prev).toBe(list.head);
+			expect(list.n).toBe(1);
+		});
+
+		it('adds a Node to end of list', function() {
+			list.insert(node2);
+			var node = list.head.next;
+			expect(node.value).toBe(2);
+			expect(node.next).toBe(list.head);
+			expect(node.prev).toBe(list.head);
+			expect(list.n).toBe(2);
+		});
+
+		it('links Node between last and first', function() {
+			list.insert(node3);
+			var node = list.head.next;
+			expect(node.value).toBe(2);
+			expect(node.next).toBe(node3);
+			expect(node.prev).toBe(node1);
+			var node = node.next;
+			expect(node.value).toBe(3);
+			expect(node.next).toBe(node1);
+			expect(node.prev).toBe(node2);
+			expect(list.n).toBe(3);
+		});
 	});
 });
+
